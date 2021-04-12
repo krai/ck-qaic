240c458b817024bbd0083cca260e312a41b404fb
Arjun <arjun@krai.ai>
2021-04-12 09:56:32 +0100

    fix

--- package/model-qaic-calibrate/install.sh
+++ package/model-qaic-calibrate/install.sh
@@ -63,8 +63,8 @@ if [[ -n ${_AIMET_MODEL} ]]; then
      PYTHONPATH=${CK_ENV_MLPERF_INFERENCE}/vision/classification_and_detection/python
      AIMET_PATH=${CK_ENV_LIB_AIMET}/../../../lib/x86_64-linux-gnu:${CK_ENV_LIB_AIMET}/../../../lib/python
      export PYTHONPATH=${AIMET_PATH}:$PYTHONPATH
-     #export LD_LIBRARY_PATH=${AIMET_PATH}:$LD_LIBRARY_PATH
-     export LD_LIBRARY_PATH=${AIMET_PATH}
+     export LD_LIBRARY_PATH=${AIMET_PATH}:$LD_LIBRARY_PATH
+     #export LD_LIBRARY_PATH=${AIMET_PATH}
      cd ${AIMET_RUN}
      rm -rf output
      rm -rf preprocessed
