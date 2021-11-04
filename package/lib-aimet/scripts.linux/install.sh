acd41a11816cb0dd57cbfa5bb599a4e5bd9d633a
Arjun <arjunsuresh1987@gmail.com>
2021-11-04 22:06:51 +0530

    Moved lib-aimet

--- package/lib-aimet/scripts.linux/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd src
-mkdir -p ./ThirdParty/googletest
-pushd ./ThirdParty/googletest
-git clone https://github.com/google/googletest.git -b release-1.8.0 googletest-release-1.8.0
-popd
