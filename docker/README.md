1bad975f81e2e556e4f639ee303bc2962fbf7946
Anton Lokhmotov <anton@krai.ai>
2021-11-06 23:42:41 +0000

    Update Docker README and build.sh.

--- docker/README.md
+++ docker/README.md
@@ -15,55 +15,64 @@ interface, and can be set up in an automated way.
 ### Set up CK
 
 ```
-$ python3 -m pip install ck==1.55.5
+python3 -m pip install ck==1.55.5
 ```
 
 ### Set up CK paths
 
-Place the following into your `~/.bashrc`:
+Place the following into your `~/.bashrc` (with default CK settings):
 
 ```
+export CK_REPOS=$HOME/CK
+export CK_TOOLS=$HOME/CK-TOOLS
+export CK_EXPERIMENT_REPO=mlperf_2_0.$(hostname).$USER
+export CK_EXPERIMENT_DIR=$CK_EXPERIMENT_REPO/experiment
 export PATH=$HOME/.local/bin:$PATH
+```
+
+or (on systems in the Austin lab):
+```
 export CK_REPOS=/local/mnt/workspace/$USER/CK-REPOS
 export CK_TOOLS=/local/mnt/workspace/$USER/CK-TOOLS
-export CK_EXPERIMENT_DIR=$CK_REPOS/mlperf.$(hostname)/experiment
+export CK_EXPERIMENT_REPO=mlperf_2_0.$(hostname).$USER
+export CK_EXPERIMENT_DIR=$CK_EXPERIMENT_REPO/experiment
+export PATH=$HOME/.local/bin:$PATH
 ```
 
 Init it:
 
 ```
-$ source ~/.bashrc
+source ~/.bashrc
 ```
 
 ### Create a repository for experimental data
 
-```
-$ ck add repo:mlperf.$(hostname) --quiet && \
-  ck add mlperf.$(hostname):experiment:dummy --common_func && \
-  ck rm  mlperf.$(hostname):experiment:dummy --force
-```
-
-Experiments will land into this repository as if by magic!
 
 ```
-$ ck list repo:mlperf.*
-mlperf.aus655-pci-bowie
+ck add repo:$CK_EXPERIMENT_REPO --quiet && \
+ck add $CK_EXPERIMENT_REPO:experiment:dummy --common_func && \
+ck rm  $CK_EXPERIMENT_REPO:experiment:dummy --force
+ck list repo:mlperf_2_0.*
 ```
 
-### Make the repository group-writable
+Experiment entries will land into this repository as if by magic!
 
-All files under `$CK_EXPERIMENT_DIR` must be group-writable:
+### Make the experiment directory group-writable
+
+All files created under `$CK_EXPERIMENT_DIR` must be group-writable:
 
 ```
-$ chgrp qaic $CK_EXPERIMENT_DIR -R && chmod g+ws $CK_EXPERIMENT_DIR -R
+chgrp -R qaic $CK_EXPERIMENT_DIR && \
+chmod -R g+ws $CK_EXPERIMENT_DIR && \
+setfacl -R -d -m group:qaic:rwx $CK_EXPERIMENT_DIR
 ```
 
 ### Run a quick test
 
 ```
 $ docker run --privileged \
---user=krai:kraig --group-add $(cut -d: -f3 < <(getent group qaic)) \
---volume ${CK_EXPERIMENTS}:/home/krai/CK_REPOS/local/experiment \
+--user=krai:kraig --group-add $(getent group qaic | cut -d: -f3) \
+--volume ${CK_EXPERIMENT_DIR}:/home/krai/CK_REPOS/local/experiment \
 --rm krai/mlperf.resnet50.full.centos7:1.5.6 \
 "ck run cmdgen:benchmark.image-classification.qaic-loadgen --verbose \
 --sut=r282_z93_q1 --sdk=1.5.6 --model=resnet50 \
@@ -76,7 +85,7 @@ accuracy=75.800%, good=379, total=500
 ### Check the results
 
 ```
-$ ck list mlperf.$(hostname):experiment:mlperf*resnet50*accuracy*dataset_size.500-*
+$ ck list $CK_EXPERIMENT_REPO:experiment:mlperf*resnet50*accuracy*dataset_size.500-*
 mlperf-closed-r282_z93_q1-qaic-v1.5.6-aic100-resnet50.pcie.16nsp-resnet50-offline-accuracy-dataset_size.500-preprocessed_using.opencv-buffer_size.500
 
 $ grep accuracy\":\ 75 $(ck find experiment:mlperf*resnet50*accuracy*dataset_size.500-*)/*.0001.json
--- docker/build.sh
+++ docker/build.sh
@@ -33,18 +33,18 @@
 #
 
 if [[ $# < 1 ]]; then
-   echo 'Please enter model name to build the docker image (One among bert, resnet50, ssd-resnet34, ssd-mobilenet)';
-   exit 1;
+  echo "Please enter the model name to build the Docker image for (one of: bert, resnet50, ssd-resnet34, ssd-mobilenet).";
+  exit 1;
 fi
 
 MODEL=$1
 
-echo "Building docker image for $MODEL";
+echo "Building the Docker image for '${MODEL}'";
 
 _BASE_OS=${BASE_OS:-centos7}
 _BASE_IMAGE=${BASE_IMAGE:-qran-${_BASE_OS}}
 _SDK_VER=${SDK_VER:-1.5.9}
-_PYTHON_VER=${PYTHON_VER:-3.8.11}
+_PYTHON_VER=${PYTHON_VER:-3.8.12}
 _GCC_MAJOR_VER=${GCC_MAJOR_VER:-10}
 _DEBUG_BUILD=${DEBUG_BUILD:-no}
 
@@ -74,10 +74,10 @@ _USER_ID=${USER_ID:-2000}
 if [[ ${_CK_QAIC_PERCENTILE_CALIBRATION} == 'yes' ]]; then
   _DEBUG_BUILD=yes
 fi
- 
+
 if [[ ${_DEBUG_BUILD} != 'no' ]]; then tag_suffix='_DEBUG'; else tag_suffix=''; fi
 read -d '' CMD <<END_OF_CMD
-cd $(ck find ck-qaic:docker:$MODEL) && \
+cd $(ck find ck-qaic:docker:${MODEL}) && \
 time docker build \
 --build-arg BASE_IMAGE=${_BASE_IMAGE} \
 --build-arg SDK_VER=${_SDK_VER} \
@@ -98,11 +98,11 @@ if [ -z "${DRY_RUN}" ]; then
 fi
 
 if [[ ${_CK_QAIC_PERCENTILE_CALIBRATION} == 'yes' ]]; then
-   CONTAINER=`docker run -dt --privileged --user=krai:kraig --group-add $(cut -d: -f3 < <(getent group qaic))  --rm ${DOCKER_IMAGE_NAME}:${_SDK_VER}${tag_suffix} bash`
-   if [[ ${MODEL} == "bert" ]]; then 
-      docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,bert-99 --force'
-      docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh bert bert-99 ${_SDK_VER};'
-      docker exec $CONTAINER /bin/bash -c 'rm -rf \
+  CONTAINER=$(docker run -dt --privileged --user=krai:kraig --group-add $(getent group qaic | cut -d: -f3) --rm ${DOCKER_IMAGE_NAME}:${_SDK_VER}${tag_suffix} bash)
+  if [[ ${MODEL} == "bert" ]]; then
+    docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,bert-99 --force'
+    docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh bert bert-99 ${_SDK_VER};'
+    docker exec $CONTAINER /bin/bash -c 'rm -rf \
 $(ck find repo:ctuning-programs)/* \
 $(ck find repo:ck-crowdtuning-platforms)/* \
 $(ck locate env --tags=mlperf,inference,source)/inference/.git \
@@ -112,13 +112,13 @@ $(ck locate env --tags=tool,cmake)/cmake*/Tests \
 $(ck locate env --tags=tool,cmake)/cmake*/Source \
 $(ck locate env --tags=tool,cmake)/cmake*/Utilities \
 $(ck locate env --tags=model,bert-packed)/*;'
-   fi
+  fi
 
-   if [[ ${MODEL} == "resnet50" ]]; then
-      docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,resnet50.pcie.16nsp --force'
-      docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh resnet50 resnet50.pcie.16nsp ${_SDK_VER};'
-      docker exec $CONTAINER /bin/bash -c 'rm -rf \
- find repo:ctuning-programs)/* \
+  if [[ ${MODEL} == "resnet50" ]]; then
+    docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,resnet50.pcie.16nsp --force'
+    docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh resnet50 resnet50.pcie.16nsp ${_SDK_VER};'
+    docker exec $CONTAINER /bin/bash -c 'rm -rf \
+find repo:ctuning-programs)/* \
 $(ck find repo:ck-crowdtuning-platforms)/* \
 $(ck locate env --tags=mlperf,inference,source)/inference/.git \
 $(ck locate env --tags=python-package,tensorflow)/* \
@@ -128,12 +128,12 @@ $(ck locate env --tags=tool,cmake)/cmake*/Tests \
 $(ck locate env --tags=tool,cmake)/cmake*/Source \
 $(ck locate env --tags=tool,cmake)/cmake*/Utilities \
 $(ck locate env --tags=model,fix_input_shape)/*;'
-   fi
+  fi
 
-   if [[ ${MODEL} == "ssd-resnet34" ]]; then
-      docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,ssd-resnet34 --force'
-      docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh ssd-resnet34 ssd-resnet34.pcie.16nsp ${_SDK_VER};'
-      docker exec $CONTAINER /bin/bash -c 'rm -rf \
+  if [[ ${MODEL} == "ssd-resnet34" ]]; then
+    docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,ssd-resnet34 --force'
+    docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh ssd-resnet34 ssd-resnet34.pcie.16nsp ${_SDK_VER};'
+    docker exec $CONTAINER /bin/bash -c 'rm -rf \
 $(ck find repo:ctuning-programs)/* \
 $(ck find repo:ck-crowdtuning-platforms)/* \
 $(ck locate env --tags=mlperf,inference,source)/inference/.git \
@@ -146,12 +146,12 @@ $(ck locate env --tags=model,onnx,no-nms)/* \
 $(ck locate env --tags=dataset,coco.2017,original)/annotations/*train* \
 $(ck locate env --tags=dataset,coco.2017,original)/val2017/* && \
 touch $(ck locate env --tags=dataset,coco.2017,original)/val2017/000000000139.jpg'
-   fi
+  fi
 
-   if [[ ${MODEL} == "ssd-mobilenet" ]]; then
-      docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,ssd-mobilenet --force'
-      docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh ssd-mobilenet ssd-mobilenet.pcie.16nsp ${_SDK_VER};'
-      docker exec $CONTAINER /bin/bash -c 'ck clean env --tags=dataset,coco.2017,train --force && rm -rf \
+  if [[ ${MODEL} == "ssd-mobilenet" ]]; then
+    docker exec $CONTAINER /bin/bash -c  'ck clean env --tags=compiled,ssd-mobilenet --force'
+    docker exec $CONTAINER /bin/bash -c  '$(ck find repo:ck-qaic)/package/model-qaic-compile/percentile-calibration.sh ssd-mobilenet ssd-mobilenet.pcie.16nsp ${_SDK_VER};'
+    docker exec $CONTAINER /bin/bash -c 'ck clean env --tags=dataset,coco.2017,train --force && rm -rf \
 $(ck find repo:ctuning-programs)/* \
 $(ck find repo:ck-crowdtuning-platforms)/* \
 $(ck locate env --tags=mlperf,inference,source)/inference/.git \
@@ -164,9 +164,9 @@ $(ck locate env --tags=model,pytorch,for.qaic)/* \
 $(ck locate env --tags=dataset,coco.2017,original)/annotations/*train* \
 $(ck locate env --tags=dataset,coco.2017,original)/val2017/* && \
 touch $(ck locate env --tags=dataset,coco.2017,original)/val2017/000000000139.jpg'
-   fi
-   docker exec $CONTAINER /bin/bash -c 'ck rm experiment:* --force'
-   docker commit $CONTAINER ${DOCKER_IMAGE_NAME}:${_SDK_VER}'_pc'
+  fi
+  docker exec $CONTAINER /bin/bash -c 'ck rm experiment:* --force'
+  docker commit $CONTAINER ${DOCKER_IMAGE_NAME}:${_SDK_VER}'_pc'
 fi
 
 echo "Done."
