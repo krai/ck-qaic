8c189fc1b7dc98122335570c99571ba9b8ff4d4d
Arjun Suresh <arjunsuresh1987@gmail.com>
2022-01-25 17:36:28 +0000

    Update ck_init.sh

--- script/setup.docker/ck_init.sh
+++ script/setup.docker/ck_init.sh
@@ -2,15 +2,22 @@
 #Usage: WORKSPACE=/local/mnt/workspace ./ck_init.sh
 WORKSPACE=${WORKSPACE:-$HOME/MLC2.0}
 
-echo -n "\
 export CK_PYTHON=${CK_PYTHON:-$(which python3.8)}
 export CK_WORKSPACE=$WORKSPACE
 export CK_TOOLS=$WORKSPACE/$USER/CK-TOOLS
 export CK_REPOS=$WORKSPACE/$USER/CK-REPOS
 export CK_EXPERIMENT_REPO=mlperf_v2.0.$(hostname).$USER
 export CK_EXPERIMENT_DIR=$CK_REPOS/$CK_EXPERIMENT_REPO/experiment
-export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
-source $HOME/.bashrc
+export PATH=$HOME/.local/bin:$PATH
+
+echo -n "\
+export CK_PYTHON=${CK_PYTHON}
+export CK_WORKSPACE=$WORKSPACE
+export CK_TOOLS=$CK-TOOLS
+export CK_REPOS=$CK-REPOS
+export CK_EXPERIMENT_REPO=$CK_EXPERIMENT_REPO
+export CK_EXPERIMENT_DIR=$CK_EXPERIMENT_DIR
+export PATH=$PATH" >> $HOME/.bashrc
 
 sudo mkdir -p $CK_WORKSPACE/$USER && sudo chown $USER:qaic $CK_WORKSPACE/$USER
 
