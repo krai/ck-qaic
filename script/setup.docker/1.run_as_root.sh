fb4f98ab64c72f0c006e675b05489457471beeb6
Arjun <arjun@krai.ai>
2022-01-25 14:37:14 +0000

    1.run_as_root_centos7 added

---
+++ script/setup.docker/1.run_as_root.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install system-level packages via 'yum'.
+yum upgrade -y
+yum install -y make which patch vim git wget zip unzip openssl-devel bzip2-devel libffi-devel
+yum install -y epel-release htop tmux sudo
+yum install -y dnf
+yum clean all
+# Install system-level packages via 'dnf'.
+dnf install -y libarchive cmake
+dnf install -y scl-utils
+dnf install -y gcc-toolset-11-gcc-c++
+
+# Install Python >= 3.7 from source.
+export PYTHON_VERSION=3.8.12
+cd /usr/src \
+&& wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz \
+&& tar xzf Python-${PYTHON_VERSION}.tgz \
+&& rm -f Python-${PYTHON_VERSION}.tgz \
+&& cd /usr/src/Python-${PYTHON_VERSION} \
+&& ./configure --enable-optimizations && make -j8 altinstall \
+&& rm -rf /usr/src/Python-${PYTHON_VERSION}*
+
+# Create group 'qaic'.
+groupadd -f qaic
+# Create group 'krai'.
+groupadd -f krai
+# Create user 'krai'.
+useradd -m -g krai -s /bin/bash krai
+# Add user 'krai' to some groups.
+usermod -aG qaic,root,wheel krai
+# Do not ask user 'krai' for 'sudo' password.
+echo "krai ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Fix permissions on the 'sudo' command.
+chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
