a7e5d669d589d94a92e76dae9ff127a8ad255af8
Ekaterina Selezneva <ekaterina@krai.ai>
2022-04-29 11:41:51 +0100

    Change name of zip archive to comply with the submission process + make its creation optional

--- script/run/run_end.sh
+++ script/run/run_end.sh
@@ -7,11 +7,12 @@ if [[ ${_docker} == 'yes' ]]; then
   enabled ssd_mobilenet && docker kill $CONTAINER_ID_SSD_MOBILENET
 fi
 
-if [[ $DRY_RUN != 'yes' ]]; then
+ZIP_EXPERIMENT=${ZIP_EXPERIMENT:-'no'}
+if [[ $ZIP_EXPERIMENT == 'yes' ]]; then
   mkdir -p $HOME/krai_experiment_results/$SDK_VER
   cd $HOME/krai_experiment_results/$SDK_VER
-  rm -f $CK_EXPERIMENT_REPO'_'${_reposuffix}$SDK_VER.zip
-  ck zip $CK_EXPERIMENT_REPO:experiment:*"closed-${SUT}-"*${SDK_VER}* --archive_name=$CK_EXPERIMENT_REPO'_'${_reposuffix}$SDK_VER.zip
+  rm -f mlperf_v${MLPERF_VER}-closed-${SUT}-qaic-v${SDK_VER}.zip
+  ck zip $CK_EXPERIMENT_REPO:experiment:*"closed-${SUT}-"*${SDK_VER}* --archive_name=mlperf_v${MLPERF_VER}-closed-${SUT}-qaic-v${SDK_VER}.zip
   if [[ $RCLONE_COPY == "yes" ]]; then
     rclone copy $CK_EXPERIMENT_REPO'_'${_reposuffix}$SDK_VER.zip rclone:experiments/$SDK_VER/
   fi
