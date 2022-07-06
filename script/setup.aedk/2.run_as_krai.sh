bc9000543dd6cd76f401383ca2b23e1a61662c66
elim <elim@krai.ai>
2022-07-06 17:01:15 +0100

    Add user-changeable parameters and error checks for 2.run_as_krai.centos.sh; Separate out 3.install_benchmark.

--- script/setup.aedk/2.run_as_krai.sh
+++ script/setup.aedk/2.run_as_krai.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
 
-function exit_if_error() {
-  if [ "${?}" != "0" ]; then
-    echo ""
-    echo "ERROR: $1"
-    exit 1
-  fi
-}
-
+_PYTHON_VERSION=${PYTHON_VERSION:-3.9.13}
 _DEVICE_OS=${DEVICE_OS:-centos}
-# _BENCHMARKS = ${BENCHMARKS:-("image_classification" "object_detection" "language_processing")}
-_BENCHMARKS=${BENCHMARKS:-("image_classification" "object_detection" "language_processing")}
-_DEVICE_DATASETS_DIR=${DEVICE_DATASETS_DIR:-"/data"}
 _CK_VER=${CK_VER:-2.6.1}
+_INSTALL_PYTHON_DEPENDENCY=${INSTALL_PYTHON_DEPENDENCY:-1}
+_INSTALL_LOADGEN=${INSTALL_LOADGEN:-1}
 
-echo " Setting Parameters:"
-echo "- BENCHMARKS=${_BENCHMARKS}"
-echo "- DEVICE_DATASETS_DIR=${_DEVICE_DATASETS_DIR}"
-echo "- CK_VER =${_CK_VER}"
-echo
+. run_common.sh
+
+echo "Running '$0'"
+print_variables "${!_@}"
 
 # Set up environment
 if [[ -z $(grep "# CK-QAIC." ~/.bashrc) ]]; then
   echo "Adding CK-QAIC environment to '~/.bashrc'."
+  _PYTHON_VERSION_ARR=($(echo ${_PYTHON_VERSION} | tr "." " "))
+  _PYTHON_MAJOR=${_PYTHON_VERSION_ARR[0]}
+  _PYTHON_MINOR=${_PYTHON_VERSION_ARR[1]}
+  _PYTHON_BATCH=${_PYTHON_VERSION_ARR[2]}
   echo -n "\
-
 # CK-QAIC.
-export CK_PYTHON=$(which python3.9)
+export CK_PYTHON=$(which python${_PYTHON_MAJOR}.${_PYTHON_MINOR})
 export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
-  
+
   # Centos OS Dependency
   if [[ "${_DEVICE_OS}" == centos ]]; then
-    echo -n "source scl_source enable gcc-toolset-11" >> $HOME/.bashrc
+    echo -n "\
+source scl_source enable gcc-toolset-11" >> $HOME/.bashrc
   fi
 
   source $HOME/.bashrc
@@ -54,7 +49,6 @@ $CK_PYTHON -m pip install transformers==2.4.0 --user
 
 # Install CK.
 $CK_PYTHON -m pip install ck==${_CK_VER}
-ck set kernel var.package_quiet_install=yes
 ck pull repo --url=https://github.com/krai/ck-qaic
 
 # Init CK environment.
@@ -64,15 +58,22 @@ ck detect soft:compiler.gcc --full_path=$(which gcc)
 ck detect soft:tool.cmake --full_path=$(which cmake)
 
 # Install explicit Python dependencies.
-echo "1.2x" | ck install package --tags=python-package,numpy
-ck install package --tags=python-package,absl
-ck install package --tags=python-package,cython
-ck install package --tags=python-package,opencv-python-headless
+if [[ -z _INSTALL_PYTHON_DEPENDENCY ]]; then
+  echo "Installing explicit Python dependencies."
+  echo "1.2x" | ck install package --tags=python-package,numpy
+  ck install package --tags=python-package,absl
+  ck install package --tags=python-package,cython
+  ck install package --tags=python-package,opencv-python-headless
+else
+  echo "Passing explicit Python dependencies installation."
+fi
 
 # Install LoadGen.
-ck install package --tags=mlperf,inference,source
-ck install package --tags=mlperf,loadgen,static
-ck install package --tags=mlperf,power,source
-
-# Install Benchmark dependencies.
-BENCHMARKS=${_BENCHMARKS} DEVICE_DATASETS_DIR=${_DEVICE_DATASETS_DIR} ./3.install_benchmark.sh
+if [[ -z _INSTALL_LOADGEN ]]; then
+  echo "Installing loadgen."
+  ck install package --tags=mlperf,inference,source
+  ck install package --tags=mlperf,loadgen,static
+  ck install package --tags=mlperf,power,source
+else
+  echo "Passing loadgen installation."
+fi
