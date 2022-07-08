3c58f85999184d949d4201065ce08be380bccc2f
Anton Lokhmotov <anton@krai.ai>
2022-07-08 09:59:00 +0100

    Separate path for installing from Docker.

--- script/setup.aedk/install_to_aedk.sh
+++ script/setup.aedk/install_to_aedk.sh
@@ -2,6 +2,15 @@
 
 # Usage: PASSWORD=111111 IPS=aedk1 PORTS=3231 SUT=aedk_15w bash install_to_aedk.sh
 
+
+_DOCKER=${DOCKER:-yes}
+_DOCKER_OS=${DOCKER_OS:-ubuntu}
+_SDK_VER=${SDK_VER:-1.7.1.12}
+#_WORKLOADS="resnet50 bert-99" # ssd_mobilenet ssd_resnet34
+
+# FIXME: Only works for a single workload at at time.
+_WORKLOADS="resnet50" # ssd_mobilenet ssd_resnet34
+
 _BASE_DIR=${BASE_DIR:-/data}
 _USERNAME=${USERNAME:-krai}
 
@@ -25,25 +34,37 @@ if [ -z ${SUT} ]; then
   exit -1
 fi
 
-WORKLOADS=(resnet50 bert-99) # ssd_mobilenet ssd_resnet34
-for workload in ${WORKLOADS[@]}; do
-  echo "Workload: '$workload'"
-  if [[ "$workload" == "bert-99" ]]; then
-    SCENARIOS=(offline singlestream)
-    EXTRA_SUFFIX=",quantization.calibration"
-  else
-    SCENARIOS=(offline singlestream multistream)
-    EXTRA_SUFFIX=""
-  fi
-  for scenario in ${SCENARIOS[@]}; do
-    echo "  Scenario: '$scenario'"
-    CMD="ck install package --tags=install-to-aedk --dep_add_tags.model-qaic=$model,$model.${SUT}.${scenario}${EXTRA_SUFFIX} --env.CK_AEDK_IPS='${IPS}' --env.CK_AEDK_PORTS='${PORTS}' --env.CK_AEDK_USERNAME=${_USERNAME} --env.CK_DEST_PATH=${_BASE_DIR}/${_USERNAME}/CK-TOOLS"
-    echo $CMD
-    #eval $CMD
+function install () {
+  workloads="$@"
+  for workload in ${workloads[@]}; do
+    echo "- Workload: '$workload'"
+    if [[ "$workload" == "bert-99" ]]; then
+      scenarios=(offline singlestream)
+      EXTRA_SUFFIX=",quantization.calibration"
+    else
+      scenarios=(offline singlestream multistream)
+      EXTRA_SUFFIX=""
+    fi
+    for scenario in ${scenarios[@]}; do
+      echo "  - Scenario: '$scenario'"
+      CMD="\
+ck install package --tags=install-to-aedk \
+-dep_add_tags.model-qaic=$workload,$workload.${SUT}.${scenario}${EXTRA_SUFFIX} \
+--env.CK_AEDK_IPS='${IPS}' --env.CK_AEDK_PORTS='${PORTS}' --env.CK_AEDK_USERNAME=${_USERNAME} \
+--env.CK_DEST_PATH=${_BASE_DIR}/${_USERNAME}/CK-TOOLS"
+      echo "    CMD: $CMD"
+      #eval $CMD
+    done
+    echo
   done
-  echo
-done
+}
 
-echo
+if [[ ${_DOCKER} == "yes" ]]; then
+  echo "Installing workloads from Docker ..."
+  install ${_WORKLOADS}
+else
+  echo "Installing workloads ..."
+  install ${_WORKLOADS}
+fi
+
 echo "Done (installing workloads)."
-echo
