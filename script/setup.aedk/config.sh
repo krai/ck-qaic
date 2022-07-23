44e790d88e33bd7ab275a0e0667ae31f0846ead0
Anton Lokhmotov <anton@krai.ai>
2022-07-23 14:38:33 +0100

    Update aedk template config.

--- script/setup.aedk/config.sh
+++ script/setup.aedk/config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export DEVICE_IP=rb6
-export DEVICE_PORT=3241
-export HOST_DATASETS_DIR="/datasets"
+export DEVICE_IP=aedk3
+export DEVICE_PORT=3233
 export DEVICE_BASE_DIR="/data"
 export DEVICE_GROUP="krai"
 export DEVICE_USER="krai"
-export DEVICE_DATASETS_DIR=${DEVICE_BASE_DIR}/${DEVICE_USER}
 export DEVICE_OS=centos
 export DEVICE_OS_OVERRIDE=no
+export DEVICE_DATASETS_DIR=${DEVICE_BASE_DIR}/${DEVICE_USER}
+export HOST_DATASETS_DIR="/datasets"
 export PYTHON_VERSION=3.9.13
 export INSTALL_BENCHMARK_RESNET50=yes
 export INSTALL_BENCHMARK_BERT=yes
