c8dee53502aa35875542d11f6da038f9f43e7161
krai <info@krai.ai>
2022-09-29 06:56:19 -0500

    Update SDK installation.
    On the new RB6 BSP, the device model name ends with 'RB6 EVT V2'.
    Also, use DEVICE_BASE_DIR for SDK_DIR.

--- script/setup.aedk/install_platform_sdk.sh
+++ script/setup.aedk/install_platform_sdk.sh
@@ -13,8 +13,12 @@ else
   _OS="centos"
 fi
 
+_DEVICE_BASE_DIR=${DEVICE_BASE_DIR:-/home}
+_DEVICE_USER=${DEVICE_USER:-krai}
+_DEVICE_GROUP=${DEVICE_GROUP:-krai}
+
+_SDK_DIR=${SDK_DIR:-"${_DEVICE_BASE_DIR}/${_DEVICE_USER}"}
 _SDK_VER=${SDK_VER:-1.7.1.12}
-_SDK_DIR=${SDK_DIR:-/data/krai}
 
 _PLATFORM_SDK=${PLATFORM_SDK:-"${_SDK_DIR}/qaic-platform-sdk-${_ARCH}-${_OS}-${_SDK_VER}.zip"}
 echo ${_PLATFORM_SDK}
@@ -33,8 +37,8 @@ cd "$(dirname ${_PLATFORM_SDK})/qaic-platform-sdk-${_SDK_VER}/${_ARCH}/${_OS}"
 # Devices with perf kernel.
 if [[ $(uname -r) == *perf ]]; then
   _DEVICE_MODEL=$(cat /proc/device-tree/model | tr -d '\0')
-  # Ubuntu-based devices: Gloria, RB6, EB6 (also ends with RB6).
-  if [[ ${_OS} == "ubuntu" ]] && [[ ${_DEVICE_MODEL} == *Gloria || ${_DEVICE_MODEL} == *RB6 ]]; then
+  # Ubuntu-based devices: Gloria (ends with 'Gloria'), RB6 (contains 'RB6'), EB6 (ends with 'RB6').
+  if [[ ${_OS} == ubuntu ]] && [[ ${_DEVICE_MODEL} == *Gloria || ${_DEVICE_MODEL} == *RB6* ]]; then
     echo "yes" | rm deb/qaic-kmd_${_SDK_VER}_arm64.deb
     cp deb-perf/qaic-kmd_${_SDK_VER}_arm64.deb deb/
   fi
@@ -47,16 +51,17 @@ fi
 echo "yes" | sudo ./uninstall.sh
 sudo ./install.sh
 
+# Create dir for logs, writable by user group.
 _LOGS_DIR=${LOGS_DIR:-/opt/qti-aic/logs}
-_DEVICE_GROUP=${DEVICE_GROUP:-krai}
-_DEVICE_USER=${DEVICE_USER:-krai}
-
 sudo mkdir -p ${_LOGS_DIR}
 sudo chgrp ${_DEVICE_GROUP} ${_LOGS_DIR}
 sudo chmod g+ws ${_LOGS_DIR}
 sudo setfacl -R -d -m group:${_DEVICE_GROUP}:rw ${_LOGS_DIR}
+
+# Add user to 'qaic' group.
 sudo usermod -aG qaic ${_DEVICE_USER}
 
+# Make version utility executable by 'qaic' group.
 sudo chgrp qaic /opt/qti-aic/tools/qaic-version-util
 
 echo
