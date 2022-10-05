2970d892877b12ed57461e1b097303ee304bf8a3
krai <info@krai.ai>
2022-10-05 11:22:45 +0100

    Add aedk-side installation for ssd* models.

--- script/setup.aedk/4.install_workloads.sh
+++ script/setup.aedk/4.install_workloads.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # For locating ImageNet.
-_DEVICE_DATASETS_DIR=${DEVICE_DATASETS_DIR:-"${HOME}"}
+_DEVICE_DATASETS_DIR=${DEVICE_DATASETS_DIR:-${HOME}}
 _DEVICE_IMAGENET_DIR=${DEVICE_IMAGENET_DIR:-dataset-imagenet-ilsvrc2012-val}
 
-_INSTALL_WORKLOAD_BERT=${INSTALL_WORKLOAD_BERT:-"yes"}
-_INSTALL_WORKLOAD_RESNET50=${INSTALL_WORKLOAD_RESNET50:-"yes"}
-_INSTALL_WORKLOAD_RETINANET=${INSTALL_WORKLOAD_RETINANET:-"yes"}
+_INSTALL_WORKLOAD_BERT=${INSTALL_WORKLOAD_BERT:-no}
+_INSTALL_WORKLOAD_RESNET50=${INSTALL_WORKLOAD_RESNET50:-no}
+_INSTALL_WORKLOAD_RETINANET=${INSTALL_WORKLOAD_RETINANET:-no}
+_INSTALL_WORKLOAD_SSD_RESNET34=${INSTALL_WORKLOAD_SSD_RESNET34:-no}
+_INSTALL_WORKLOAD_SSD_MOBILENET=${INSTALL_WORKLOAD_SSD_MOBILENET:-no}
 
 . common.sh
 
@@ -43,28 +45,56 @@ if [[ "${_INSTALL_WORKLOAD_BERT}" == "yes" ]]; then
   # Install implicit dependencies via pip.
   source $HOME/.bashrc
   exit_if_empty ${CK_PYTHON} "Please set CK_PYTHON first!"
-  ${CK_PYTHON} -m pip install --user onnx-simplifier
-  ${CK_PYTHON} -m pip install --user tokenization
-  #${CK_PYTHON} -m pip install --user nvidia-pyindex
-  #${CK_PYTHON} -m pip install --user onnx-graphsurgeon==0.3.11
+  #${CK_PYTHON} -m pip install --user tokenization
 
   # Install explicit dependencies via CK (also via pip, but register with CK at the same time).
   ck install package --tags=python-package,onnx --force_version=1.8.1 --quiet
   ck install package --tags=lib,python-package,pytorch --force_version=1.8.1 --quiet
   ck install package --tags=lib,python-package,transformers --force_version=2.4.0
 
-  # Download the SQuAD v1.1 dataset.
+  # Download and preprocess the SQuAD v1.1 dataset.
   ck install package --tags=dataset,squad,raw,width.384 --quiet
   ck install package --tags=dataset,calibration,squad,pickle,width.384 --quiet
 fi
 
-if [[ "${_INSTALL_WORKLOAD_RETINANET}" == "yes" ]]; then
-  python3 -m pip install fiftyone torch torchvision
-  ck install package --tags=dataset,openimages,original,validation
-  ck install package --tags=dataset,preprocessed,openimages,for.retinanet.onnx.preprocessed.quantized,validation,full
+if [[ "${_INSTALL_WORKLOAD_RETINANET}" == "yes" ]] || [[ "${_INSTALL_WORKLOAD_SSD_RESNET34}" == "yes" ]] || [[ "${_INSTALL_WORKLOAD_SSD_MOBILENET}" == "yes" ]]; then
+  if [[ ! -d $(ck locate env --tags=opencv-python-headless) ]]; then
+    ck install package --tags=opencv-python-headless
+  fi
   ck install package --tags=tool,coco,master
   ck install package --tags=lib,nms,abp,master
 fi
 
+if [[ "${_INSTALL_WORKLOAD_RETINANET}" == "yes" ]]; then
+  python3 -m pip install fiftyone torch torchvision
+  if [[ ! -d $(ck locate env --tags=dataset,openimages,original,validation) ]]; then
+    ck install package --tags=dataset,openimages,original,validation
+  fi
+  if [[ ! -d $(ck locate env --tags=dataset,preprocessed,openimages,for.retinanet.onnx.preprocessed.quantized,validation,full) ]]; then
+    ck install package --tags=dataset,preprocessed,openimages,for.retinanet.onnx.preprocessed.quantized,validation,full
+  fi
+fi
+
+if [[ "${_INSTALL_WORKLOAD_SSD_RESNET34}" == "yes" ]] || [[ "${_INSTALL_WORKLOAD_SSD_MOBILENET}" == "yes" ]]; then
+  if [[ ! -d $(ck locate env --tags=dataset,coco,original) ]]; then
+    ck install package --tags=dataset,coco.2017,val --quiet
+  fi
+fi
+
+if [[ "${_INSTALL_WORKLOAD_SSD_RESNET34}" == "yes" ]]; then
+  if [[ ! -d $(ck locate env --tags=dataset,object-detection,for.ssd_resnet34.onnx.preprocessed.quantized,using-opencv,full,validation) ]]; then
+    ck install package --dep_add_tags.lib-python-cv2=opencv-python-headless --quiet \
+    --tags=dataset,object-detection,for.ssd_resnet34.onnx.preprocessed.quantized,using-opencv,full --extra_tags=validation
+  fi
+fi
+
+if [[ "${_INSTALL_WORKLOAD_SSD_MOBILENET}" == "yes" ]]; then
+  if [[ ! -d $(ck locate env --tags=dataset,object-detection,for.ssd_mobilenet.onnx.preprocessed.quantized,using-opencv,full,validation) ]]; then
+    ck install package --dep_add_tags.lib-python-cv2=opencv-python-headless --quiet \
+    --tags=dataset,object-detection,for.ssd_mobilenet.onnx.preprocessed.quantized,using-opencv,full,validation
+  fi
+fi
+
 echo
-echo "Done."
+echo "DONE (installing workloads)."
+echo
